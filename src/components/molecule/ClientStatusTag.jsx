ea682a03370198046862e58e15e01ea391887c7e
Milan de Graaf <mmaster.milan@gmail.com>
2018-12-09 01:51:51 +0100

    Proper status for client based on the activity in sprints

--- src/components/molecule/ClientStatusTag.jsx
+++ src/components/molecule/ClientStatusTag.jsx
@@ -3,17 +3,37 @@ import React from 'react';
 import {StatusTag} from './StatusTag'
 import {ProjectConsumer} from '../providers/Project'
 import {SprintConsumer} from '../providers/Sprint'
+import SprintStatusEnum from '../../lib/SprintStatusEnum'
 
 const ClientStatusEnum = {
     INACTIVE: 'INACTIVE',
+    WAITING: 'WAITING',
     ACTIVE: 'ACTIVE',
 };
 
-const getClientStatus = (projects, sprints) => {
-    if(projects.length === 0 || sprints.length === 0){
+const isWaiting = sprint => [SprintStatusEnum.FUTURE, SprintStatusEnum.FINISHED].includes(sprint.status);
+
+const isActive = sprint => [
+    SprintStatusEnum.PLANNED,
+    SprintStatusEnum.ACTIVE,
+    SprintStatusEnum.IN_REVIEW,
+    SprintStatusEnum.FINALIZING,
+].includes(sprint.status);
+
+const getClientStatus = (sprints) => {
+    if(sprints.length === 0){
         return ClientStatusEnum.INACTIVE;
     }
-    return ClientStatusEnum.ACTIVE;
+
+    if(sprints.every(sprint => isWaiting(sprint))){
+        return ClientStatusEnum.WAITING;
+    }
+
+    if(sprints.some(sprint => isActive(sprint))){
+        return ClientStatusEnum.ACTIVE;
+    }
+
+    return ClientStatusEnum.INACTIVE;
 };
 
 export const ClientStatusTag = ({ client, onChange }) => (
@@ -22,7 +42,7 @@ export const ClientStatusTag = ({ client, onChange }) => (
             <SprintConsumer projectIds={projects.map(project => project.id)}>
                 {sprints => (
                     <StatusTag
-                        status={getClientStatus(projects, sprints)}
+                        status={getClientStatus(sprints)}
                         values={Object.values(ClientStatusEnum)}
                         onChange={onChange}
                         noChange={true}
